337dff5e5e43d73d848686275aba231deb7330fb
dilaeksii <dilaeksii@gmail.com>
2026-03-28 13:29:21 +0300

    s18d2 finished

--- src/test/java/com/workintech/sqldmlprocedures/SqlDmlProceduresApplicationTests.java
+++ src/test/java/com/workintech/sqldmlprocedures/SqlDmlProceduresApplicationTests.java
@@ -10,8 +10,10 @@ import com.workintech.sqldmlprocedures.repository.TurRepository;
 import com.workintech.sqldmlprocedures.repository.YazarRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 import java.util.stream.Collectors;
